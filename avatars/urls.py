8f4a5e5b2c6ee70329fd9f5df78f24ee69ed7274
m0r13 <moritz.hilscher@gmail.com>
2015-01-24 15:20:55 +0100

    Generate body/head avatars, also simple resizing.

--- avatars/urls.py
+++ avatars/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import patterns, url
 from . import views
 
 urlpatterns = patterns('',
-    url(r"^head/(?P<user>[a-zA-Z0-9-_]+)", views.head, {"size" : 16}, name="head"),
-    url(r"^head/(?P<user>[a-zA-Z0-9-_]+)/(?P<size>\d+)", views.head, name="head"),
+    url(r"^head/(?P<user>[a-zA-Z0-9-_]+)$", views.head, {"size" : 1}, name="head"),
+    url(r"^head/(?P<user>[a-zA-Z0-9-_]+)/(?P<size>\d+)$", views.head, name="head"),
+    
+    url(r"^body/(?P<user>[a-zA-Z0-9-_]+)$", views.body, {"size" : 1}, name="body"),
+    url(r"^body/(?P<user>[a-zA-Z0-9-_]+)/(?P<size>\d+)$", views.body, name="body"),
 )
--- avatars/views.py
+++ avatars/views.py
@@ -1,7 +1,7 @@
 import io
 import os
-import urllib
 import time
+import urllib
 
 from django.conf import settings
 from django.http.response import HttpResponse
@@ -10,10 +10,7 @@ from django.shortcuts import render
 from PIL import Image
 
 
-def gen_player_head(skin):
-    return skin.crop((8, 8, 16, 16))
-
-def head(request, size, user):
+def get_avatar(gen_func, cache_name, request, user, size):
     cache_root = getattr(settings, "AVATARS_CACHE_DIR", None)
     if cache_root is None:
         return HttpResponse("Cache dir is not set!")
@@ -28,10 +25,10 @@ def head(request, size, user):
     
     # get url of user skin and path to avatar cache file
     url = "http://skins.minecraft.net/MinecraftSkins/%s.png" % user
-    cache_dir = os.path.join(cache_root, "head")
+    cache_dir = os.path.join(cache_root, cache_name)
     if not os.path.exists(cache_dir):
         os.makedirs(cache_dir)
-    cache_file = os.path.join(cache_dir, "%s.png" % user)
+    cache_file = os.path.join(cache_dir, "%s_%d.png" % (user, size))
     
     # check if avatar is already generated in cache
     if os.path.exists(cache_file):
@@ -51,8 +48,37 @@ def head(request, size, user):
         skin = Image.open(os.path.join(os.path.dirname(__file__), "steve.png"))
     
     # now generate avatar and save it
-    avatar = gen_player_head(skin)
+    avatar = gen_func(skin)
+    avatar = avatar.resize((avatar.size[0]*size, avatar.size[1]*size))
     avatar.save(cache_file)
     
     # return generated avatar
     return HttpResponse(open(cache_file, "rb").read(), content_type="image/png")
+
+def gen_player_head(skin):
+    # head
+    head = skin.crop((8, 8, 16, 16))
+    # hat
+    hat = skin.crop((40, 8, 40+8, 8+8))
+    head.paste(hat, (0, 0), hat)
+    return head
+
+def head(request, user, size):
+    return get_avatar(gen_player_head, "head", request, user, size)
+
+def gen_player_body(skin):
+    body = Image.new("RGBA", (16, 32))
+    # head
+    body.paste(gen_player_head(skin), (4, 0))
+    # body
+    body.paste(skin.crop((20, 20, 20+8, 20+12)), (4, 8))
+    # left and right arm
+    body.paste(skin.crop((44, 20, 44+4, 20+12)), (0, 8))
+    body.paste(skin.crop((47, 20, 47+4, 20+12)), (12, 8))
+    # left and right leg
+    body.paste(skin.crop((4, 20, 4+4, 20+12)), (4, 20))
+    body.paste(skin.crop((8, 20, 8+4, 20+12)), (8, 20))
+    return body
+
+def body(request, user, size):
+    return get_avatar(gen_player_body, "body", request, user, size)
